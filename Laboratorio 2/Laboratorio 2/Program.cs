103afe1e65f84d759b7e80726209f5f2b32c60de
jimenez-jose <cosijimenez@gmail.com>
2025-09-15 19:44:12 -0500

    Laboratorio 2 completado

---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Llamamos al método
+            Sumar();
+        }
+
+        static void Sumar()
+        {
+            int variableLocal = 10;   // variable local
+            int resultado = variableLocal + 5;
+            Console.WriteLine("El resultado de la suma es: " + resultado);
+        }
+    }
+}
--- Laboratorio 2/Laboratorio 2/obj/Debug/net8.0/Laboratorio 2.AssemblyInfo.cs
+++ Laboratorio 2/Laboratorio 2/obj/Debug/net8.0/Laboratorio 2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5251280c25358e853903f409cbf5b63672f8f2")]
-[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
