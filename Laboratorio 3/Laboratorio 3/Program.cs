b41bec5c54a6c4bd61de206465e9e7004d60a8de
jimenez-jose <cosijimenez@gmail.com>
2025-09-15 20:02:01 -0500

    Laboratorio 3 completado

---
+++ Laboratorio 3/Laboratorio 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Laboratorio31
+{
+    class CalculosMatematicos
+    {
+        public int Calcular(int a, int b)
+        {
+            return (a + b) * (a - b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el primer número (a): ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número (b): ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            CalculosMatematicos calc = new CalculosMatematicos();
+            int resultado = calc.Calcular(a, b);
+
+            Console.WriteLine($"El resultado de (a+b)*(a-b) es: {resultado}");
+        }
+    }
+}
