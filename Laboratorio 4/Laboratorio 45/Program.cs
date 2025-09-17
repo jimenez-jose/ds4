ed86a3c23a1fef7f3ace86d8d2c8a1046ce8ccc1
jimenez-jose <cosijimenez@gmail.com>
2025-09-17 12:35:23 -0500

    Laboratorio 45 completado

---
+++ Laboratorio 4/Laboratorio 45/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Laboratorio45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10, c = 15;
+
+            Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+        }
+    }
+}
