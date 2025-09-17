41a860fe2c8c3ba90b44bad26f1e3eac960185ee
jimenez-jose <cosijimenez@gmail.com>
2025-09-17 12:36:17 -0500

    Laboratorio 46 completado

---
+++ Laboratorio 4/Laboratorio 46/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Laboratorio46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = Math.PI;
+
+            Console.Write("Ingrese el radio de un círculo: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            double area = PI * Math.Pow(radio, 2);
+            double circunferencia = 2 * PI * radio;
+
+            Console.WriteLine($"Área del círculo: {area}");
+            Console.WriteLine($"Circunferencia: {circunferencia}");
+        }
+    }
+}
