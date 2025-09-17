c5f4072719b9465c118f6119e2145aafcd8af109
jimenez-jose <cosijimenez@gmail.com>
2025-09-17 12:34:36 -0500

    Laboratorio 44 completado

---
+++ Laboratorio 4/Laboratorio 44/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Laboratorio44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0)
+            {
+                Console.WriteLine("El número es positivo.");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("El número es negativo.");
+            }
+            else
+            {
+                Console.WriteLine("El número es cero.");
+            }
+        }
+    }
+}
