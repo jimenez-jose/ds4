825fb6c12a11c1f60114f2285fc0c38a3c905b8f
jimenez-jose <cosijimenez@gmail.com>
2025-09-17 12:33:25 -0500

    Laboratorio 43 completado

---
+++ Laboratorio 4/Laboratorio 43/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Laboratorio43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            do
+            {
+                Console.Write("Ingrese un número mayor que 0 (0 para salir): ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero > 0)
+                {
+                    Console.WriteLine($"Ingresaste: {numero}");
+                }
+
+            } while (numero != 0);
+
+            Console.WriteLine("Programa finalizado.");
+        }
+    }
+}
