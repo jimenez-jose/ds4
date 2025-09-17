5577a85d96a6dc6256ce051b413381547c708308
jimenez-jose <cosijimenez@gmail.com>
2025-09-17 12:30:06 -0500

    Laboratorio 42 completado

---
+++ Laboratorio 4/Laboratorio 42/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Laboratorio42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número entero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            long factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"El factorial de {n} es: {factorial}");
+        }
+    }
+}
