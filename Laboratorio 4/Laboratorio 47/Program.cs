22de806be0d732335b8e10d13f5aabc6b03f5459
jimenez-jose <cosijimenez@gmail.com>
2025-09-17 12:37:17 -0500

    Laboratorio 47 completado

---
+++ Laboratorio 4/Laboratorio 47/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Laboratorio47
+{
+    class Operaciones
+    {
+        public int Sumar(int a, int b = 10) // b es opcional
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones op = new Operaciones();
+
+            Console.WriteLine("Suma con 1 parámetro (usa valor por defecto): " + op.Sumar(5));
+            Console.WriteLine("Suma con 2 parámetros: " + op.Sumar(5, 20));
+        }
+    }
+}
