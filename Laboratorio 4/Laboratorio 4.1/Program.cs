84ce86e189ab57a5bf05191f89e0061a5fb6e3b5
jimenez-jose <cosijimenez@gmail.com>
2025-09-17 12:25:28 -0500

    Laboratorio 4 completado

---
+++ Laboratorio 4/Laboratorio 4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Laboratorio41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número entero n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int i = 1;
+            while (i <= n)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
