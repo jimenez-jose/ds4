dd6768661181f6d298c4297a4d5165d7353346e6
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 18:52:12 -0500

    Laboratorio 56 completado

---
+++ Laboratorio 5/Laboratorio 56/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, string> paises = new Dictionary<string, string>
+        {
+            {"Panamá", "Ciudad de Panamá"},
+            {"España", "Madrid"},
+            {"México", "Ciudad de México"},
+            {"Argentina", "Buenos Aires"}
+        };
+
+        Console.WriteLine("Países y sus capitales:");
+        foreach (var par in paises)
+        {
+            Console.WriteLine($"País: {par.Key}, Capital: {par.Value}");
+        }
+    }
+}
