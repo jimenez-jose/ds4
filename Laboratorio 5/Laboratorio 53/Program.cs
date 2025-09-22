56af5a60c54516f4ea462bc6f05f84e3c1b2a167
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 18:48:51 -0500

    Laboratorio 53 completado

---
+++ Laboratorio 5/Laboratorio 53/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 10, 20, 30, 40, 50 };
+
+        Console.WriteLine("Elementos del arreglo:");
+        foreach (int num in numeros)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
