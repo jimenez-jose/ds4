0614fcd3286c724c3bacbd74fdf6cd0ceb365af9
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 18:44:50 -0500

    Laboratorio 51 completado

---
+++ Laboratorio 5/Laboratorio 51/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] sueldos = new double[5];
+
+        for (int i = 0; i < sueldos.Length; i++)
+        {
+            Console.Write($"Ingrese el sueldo del operario {i + 1}: ");
+            sueldos[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nSueldos ingresados:");
+        for (int i = 0; i < sueldos.Length; i++)
+        {
+            Console.WriteLine($"Operario {i + 1}: {sueldos[i]}");
+        }
+    }
+}
