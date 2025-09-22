b44e434ec25ac30db60a54bb786968f31a062e31
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 18:50:00 -0500

    Laboratorio 54 completado

---
+++ Laboratorio 5/Laboratorio 54/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] notas = { 85.5, 90.0, 78.5, 92.0, 88.5 };
+        double suma = 0;
+
+        foreach (double nota in notas)
+        {
+            suma += nota;
+        }
+
+        double promedio = suma / notas.Length;
+        Console.WriteLine($"Promedio de calificaciones: {promedio:F2}");
+    }
+}
