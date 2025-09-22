17efbc93ae8c52c00e9cef4552351dce68a61a95
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 18:51:13 -0500

    Laboratorio 55 completado

---
+++ Laboratorio 5/Laboratorio 55/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Estudiante
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Estudiante> estudiantes = new List<Estudiante>
+        {
+            new Estudiante { Nombre = "Ana", Edad = 20 },
+            new Estudiante { Nombre = "Luis", Edad = 22 },
+            new Estudiante { Nombre = "Marta", Edad = 19 }
+        };
+
+        Console.WriteLine("Lista de estudiantes:");
+        foreach (var est in estudiantes)
+        {
+            Console.WriteLine($"Nombre: {est.Nombre}, Edad: {est.Edad}");
+        }
+    }
+}
