b83b0aeb959754dc88c82dcd6e7f9468f7cc9549
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 18:47:03 -0500

    Laboratorio 52 completado

---
+++ Laboratorio 5/Laboratorio 52/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matriz = new int[3, 4];
+
+        for (int fila = 0; fila < 3; fila++)
+        {
+            for (int col = 0; col < 4; col++)
+            {
+                Console.Write($"Ingrese el valor en posición [{fila},{col}]: ");
+                matriz[fila, col] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("\nValores en la matriz:");
+        for (int fila = 0; fila < 3; fila++)
+        {
+            for (int col = 0; col < 4; col++)
+            {
+                Console.Write($"{matriz[fila, col]} \t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
