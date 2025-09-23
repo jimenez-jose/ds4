5108a20a34847c653c32a782377b4697564f008d
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 19:12:09 -0500

    Laboratorio 62 completado

---
+++ Laboratorio 6/Laboratorio 62/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Ingrese el dividendo: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el divisor: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = a / b;
+            Console.WriteLine($"Resultado: {resultado}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: No se puede dividir entre cero.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Debe ingresar solo números enteros.");
+        }
+    }
+}
