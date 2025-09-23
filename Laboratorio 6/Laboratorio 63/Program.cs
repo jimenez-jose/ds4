ebe33222ef81fb7d769f9b4768f95571128888ef
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 19:13:22 -0500

    Laboratorio 63 completado

---
+++ Laboratorio 6/Laboratorio 63/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Ingrese un número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Número ingresado: {numero}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Entrada inválida, se esperaba un número.");
+        }
+        finally
+        {
+            Console.WriteLine("Bloque finally ejecutado: Gracias por usar el programa.");
+        }
+    }
+}
