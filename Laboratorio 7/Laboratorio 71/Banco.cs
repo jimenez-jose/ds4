77c76440313e9197a952e0ceca265f8a16aad2b4
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 19:30:12 -0500

    Laboratorio 7 completado

---
+++ Laboratorio 7/Laboratorio 71/Banco.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Banco
+{
+    private Cliente cliente1;
+    private Cliente cliente2;
+    private Cliente cliente3;
+
+    public Banco()
+    {
+        cliente1 = new Cliente("Ana");
+        cliente2 = new Cliente("Luis");
+        cliente3 = new Cliente("Marta");
+    }
+
+    public void Operar()
+    {
+        cliente1.Depositar(1000);
+        cliente2.Depositar(2000);
+        cliente3.Depositar(1500);
+
+        cliente1.Extraer(500);
+        cliente3.Extraer(200);
+    }
+
+    public void DepositosTotales()
+    {
+        double total = cliente1.RetornarMonto() +
+                       cliente2.RetornarMonto() +
+                       cliente3.RetornarMonto();
+
+        cliente1.Imprimir();
+        cliente2.Imprimir();
+        cliente3.Imprimir();
+
+        Console.WriteLine($"\nEl total de dinero en el banco es: {total}");
+    }
+}
---
+++ Laboratorio 7/Laboratorio 71/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Cliente
+{
+    private string nombre;
+    private double monto;
+
+    public Cliente(string nombre)
+    {
+        this.nombre = nombre;
+        this.monto = 0;
+    }
+
+    public void Depositar(double cantidad)
+    {
+        monto += cantidad;
+    }
+
+    public void Extraer(double cantidad)
+    {
+        if (cantidad <= monto)
+            monto -= cantidad;
+        else
+            Console.WriteLine($"{nombre} no tiene suficiente saldo para extraer {cantidad}");
+    }
+
+    public double RetornarMonto()
+    {
+        return monto;
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine($"{nombre} tiene depositado: {monto}");
+    }
+}
+
