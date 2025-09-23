fd02ef172afee22528bc6080f65d312ec7eecae1
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 19:08:57 -0500

    Laboratorio 6 completado

---
+++ Laboratorio 6/Laboratorio 61/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Ingrese un número entero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"El número ingresado es: {numero}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: El valor ingresado no es un número entero válido.");
+        }
+    }
+}
