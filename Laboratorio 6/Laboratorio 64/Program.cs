688af133ef059fa0c5b27308da1ea1940fecd300
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 19:14:22 -0500

    Laboratorio 64 completado

---
+++ Laboratorio 6/Laboratorio 64/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Ingrese su edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad < 0)
+            {
+                throw new ArgumentException("La edad no puede ser negativa.");
+            }
+            else if (edad < 18)
+            {
+                throw new Exception("Debe ser mayor de edad para continuar.");
+            }
+
+            Console.WriteLine("Edad válida. Puede continuar.");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
