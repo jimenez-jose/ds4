3375374a7d7d2f70ba59ebeeb887378a3d842bad
UTP-FISC <UTP-FISC@DESKTOP-9KEN8BJ>
2025-09-22 19:33:55 -0500

    Laboratorio 72 completado

---
+++ Laboratorio 7/Laboratorio 72/Dado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Dado
+{
+    private int valor;
+    private static Random rnd = new Random();
+
+    public void Tirar()
+    {
+        valor = rnd.Next(1, 7); // valores del 1 al 6
+    }
+
+    public void Imprimir()
+    {
+        Console.Write($"{valor} ");
+    }
+
+    public int RetornarValor()
+    {
+        return valor;
+    }
+}
---
+++ Laboratorio 7/Laboratorio 72/JuegoDeDados.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class JuegoDeDados
+{
+    private Dado dado1;
+    private Dado dado2;
+    private Dado dado3;
+
+    public JuegoDeDados()
+    {
+        dado1 = new Dado();
+        dado2 = new Dado();
+        dado3 = new Dado();
+    }
+
+    public void Jugar()
+    {
+        dado1.Tirar();
+        dado2.Tirar();
+        dado3.Tirar();
+
+        Console.Write("Valores obtenidos: ");
+        dado1.Imprimir();
+        dado2.Imprimir();
+        dado3.Imprimir();
+        Console.WriteLine();
+
+        if (dado1.RetornarValor() == dado2.RetornarValor() &&
+            dado2.RetornarValor() == dado3.RetornarValor())
+        {
+            Console.WriteLine("¡Ganó!");
+        }
+        else
+        {
+            Console.WriteLine("Perdió.");
+        }
+    }
+}
+
