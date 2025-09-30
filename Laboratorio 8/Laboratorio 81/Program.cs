40e7cc59ac0c2d4111bce5ae469952506f01fc78
jimenez-jose <cosijimenez@gmail.com>
2025-09-29 19:16:04 -0500

    Laboratorio 8 completado

---
+++ Laboratorio 8/Laboratorio 81/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Laboratorio81
+{
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Nif { get; set; }
+
+        public Persona(string nombre, int edad, string nif)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Nif = nif;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}, NIF: {Nif}");
+        }
+    }
+
+    class Trabajador : Persona
+    {
+        public int Sueldo { get; set; }
+
+        public Trabajador(string nombre, int edad, string nif, int sueldo)
+            : base(nombre, edad, nif)
+        {
+            Sueldo = sueldo;
+        }
+
+        public void MostrarTrabajador()
+        {
+            MostrarInfo();
+            Console.WriteLine($"Sueldo: {Sueldo}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trabajador t1 = new Trabajador("Carlos", 30, "8-123-456", 1200);
+            t1.MostrarTrabajador();
+        }
+    }
+}
