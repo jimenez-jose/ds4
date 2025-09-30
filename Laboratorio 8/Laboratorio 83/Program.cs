4011f0b98aedcdf3d6399022aab8cd69f5a3f413
jimenez-jose <cosijimenez@gmail.com>
2025-09-29 19:24:06 -0500

    Laboratorio 83 completado

---
+++ Laboratorio 8/Laboratorio 83/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Laboratorio83
+{
+    class Program
+    {
+        static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double Suma(double a, double b)
+        {
+            return a + b;
+        }
+
+        static string Suma(string a, string b)
+        {
+            return a + " " + b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Suma(5, 10));
+            Console.WriteLine(Suma(3.5, 4.7));
+            Console.WriteLine(Suma("Hola", "Mundo"));
+        }
+    }
+}
