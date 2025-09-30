523cf0662bab5719a0bd720b35968d83c0e9e775
jimenez-jose <cosijimenez@gmail.com>
2025-09-29 19:25:06 -0500

    Laboratorio 87 completado

---
+++ Laboratorio 8/Laboratorio 87/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Laboratorio87
+{
+    sealed class Padre
+    {
+        public void Metodo()
+        {
+            Console.WriteLine("Clase Final - no se puede heredar");
+        }
+    }
+
+    // class Hijo : Padre { } ❌ Error, no se puede heredar de una clase sealed
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Padre p = new Padre();
+            p.Metodo();
+        }
+    }
+}
