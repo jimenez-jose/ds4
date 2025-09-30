dbaf229a263a56143184ddcf1dc520509afa9f44
jimenez-jose <cosijimenez@gmail.com>
2025-09-29 19:24:38 -0500

    Laboratorio 85 completado

---
+++ Laboratorio 8/Laboratorio 85/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Laboratorio85
+{
+    public partial class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+
+    public partial class Persona
+    {
+        public void Mostrar()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona { Nombre = "Luis", Edad = 20 };
+            p.Mostrar();
+        }
+    }
+}
