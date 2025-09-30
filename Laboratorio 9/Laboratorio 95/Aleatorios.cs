ad589bdc6a0a980f4f09c5a07869e1f7874daad1
jimenez-jose <cosijimenez@gmail.com>
2025-09-29 19:51:04 -0500

    Laboratorio 9 completado

---
+++ Laboratorio 9/Laboratorio 95/Aleatorios.cs
@@ -0,0 +1,17 @@
+﻿class Aleatorios
+{
+    private Random rnd = new Random();
+
+    public int[] GenerarNoRepetidos(int cantidad, int min, int max)
+    {
+        if (cantidad > (max - min + 1))
+            throw new ArgumentException("El rango no permite generar esa cantidad sin repetir.");
+
+        HashSet<int> numeros = new HashSet<int>();
+        while (numeros.Count < cantidad)
+        {
+            numeros.Add(rnd.Next(min, max + 1));
+        }
+        return new List<int>(numeros).ToArray();
+    }
+}
---
+++ Laboratorio 9/Laboratorio 95/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Aleatorios ale = new Aleatorios();
+        int[] arr = ale.GenerarNoRepetidos(10, 1, 50);
+
+        Console.WriteLine("Arreglo sin repetidos:");
+        foreach (int n in arr)
+            Console.Write(n + " ");
+    }
+}
