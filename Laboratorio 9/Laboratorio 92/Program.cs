1245c7bb839c9e6beecbbaf7e0e7e32665383d33
jimenez-jose <cosijimenez@gmail.com>
2025-09-29 19:42:45 -0500

    Laboratorio 9 completado

---
+++ Laboratorio 9/Laboratorio 92/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
