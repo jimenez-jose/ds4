d807a46bbe7dda9f12221df0723a1e9b0eca477a
jimenez-jose <cosijimenez@gmail.com>
2025-09-29 19:24:52 -0500

    Laboratorio 86 completado

---
+++ Laboratorio 8/Laboratorio 86/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Laboratorio86
+{
+    class Padre
+    {
+        public virtual void Metodo()
+        {
+            Console.WriteLine("Metodo en Padre");
+        }
+
+        public virtual sealed void MetodoFinal()
+        {
+            Console.WriteLine("Metodo Final en Padre");
+        }
+    }
+
+    class Hijo : Padre
+    {
+        // public override void MetodoFinal() { }  ❌ Error, no se puede sobrescribir
+        public override void Metodo()
+        {
+            Console.WriteLine("Metodo sobreescrito en Hijo");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hijo h = new Hijo();
+            h.Metodo();
+            h.MetodoFinal();
+        }
+    }
+}
