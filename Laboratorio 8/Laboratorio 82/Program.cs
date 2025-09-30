1fb225740ace17ff7075c8eb950820b7feab1244
jimenez-jose <cosijimenez@gmail.com>
2025-09-29 19:23:25 -0500

    Laboratorio 82 completado

---
+++ Laboratorio 8/Laboratorio 82/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Laboratorio82
+{
+    class Cuenta
+    {
+        public virtual double CalcularIntereses(double saldo)
+        {
+            return saldo * 0.01;
+        }
+    }
+
+    class CuentaAhorro : Cuenta
+    {
+        public override double CalcularIntereses(double saldo)
+        {
+            return saldo * 0.03;
+        }
+    }
+
+    class CuentaPlazoFijo : Cuenta
+    {
+        public override double CalcularIntereses(double saldo)
+        {
+            return saldo * 0.05;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta c1 = new Cuenta();
+            Cuenta c2 = new CuentaAhorro();
+            Cuenta c3 = new CuentaPlazoFijo();
+
+            Console.WriteLine("Cuenta Normal: " + c1.CalcularIntereses(1000));
+            Console.WriteLine("Cuenta Ahorro: " + c2.CalcularIntereses(1000));
+            Console.WriteLine("Cuenta Plazo Fijo: " + c3.CalcularIntereses(1000));
+        }
+    }
+}
