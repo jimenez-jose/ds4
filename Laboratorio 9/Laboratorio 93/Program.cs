c293998b598082004b20d0a0bf9ce5a025a5ebcf
jimenez-jose <cosijimenez@gmail.com>
2025-09-29 19:43:42 -0500

    Laboratorio 9 completado

---
+++ Laboratorio 9/Laboratorio 93/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Laboratorio93
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el lado A: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el lado B: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el lado C: ");
+            double c = double.Parse(Console.ReadLine());
+
+            // Verificar si es un triángulo válido
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("El triángulo es equilátero");
+                }
+                else if (a == b || a == c || b == c)
+                {
+                    Console.WriteLine("El triángulo es isósceles");
+                }
+                else
+                {
+                    Console.WriteLine("El triángulo es escaleno");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Los lados no forman un triángulo válido");
+            }
+        }
+    }
+}
