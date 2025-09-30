3828e9d4548a19b7835fb45095659b8e8570b82c
jimenez-jose <cosijimenez@gmail.com>
2025-09-29 19:46:42 -0500

    Laboratorio 94 completado

---
+++ Laboratorio 9/Laboratorio 94/Aleatorios.cs
@@ -0,0 +1,21 @@
+﻿class Aleatorios
+{
+    private Random rnd = new Random();
+
+    // Generar un número entre dos números
+    public int GenerarNumero(int min, int max)
+    {
+        return rnd.Next(min, max + 1);
+    }
+
+    // Generar un arreglo de números entre dos números aleatorios
+    public int[] GenerarArreglo(int tamano, int min, int max)
+    {
+        int[] arreglo = new int[tamano];
+        for (int i = 0; i < tamano; i++)
+        {
+            arreglo[i] = rnd.Next(min, max + 1);
+        }
+        return arreglo;
+    }
+}
---
+++ Laboratorio 9/Laboratorio 94/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Aleatorios ale = new Aleatorios();
+        Console.WriteLine("Número aleatorio: " + ale.GenerarNumero(1, 10));
+
+        int[] arr = ale.GenerarArreglo(5, 1, 20);
+        Console.WriteLine("Arreglo aleatorio:");
+        foreach (int n in arr)
+            Console.Write(n + " ");
+    }
+}
